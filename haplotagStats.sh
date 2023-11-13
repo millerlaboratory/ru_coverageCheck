0817726959613209528accef1d334484394077e0
Miranda Perez Galey Zalusky <mgaley@uw.edu>
2023-11-13 08:04:50 -0800

    fixed naming error

--- haplotagStats.sh
+++ haplotagStats.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-while getopts "i:r:" option; do
-    case $option in 
-        i) INPUT=$OPTARG ;;
-        r) TARGET=$OPTARG ;;
-    esac
-done
-
 RUSTSCRIPT="haplotagParse"
 HEADER="File\tRegion\tCoords\tDepth\tHP1\tHP2\tPercentAssigned"
-
 FMR="chrX:147900000-148000000"
 COL1A1="chr17:50150000-50250000"
 
+SEXCHECK=0;
+
+while getopts "i:r:s" option; do
+    case $option in 
+        i) INPUT=$OPTARG ;;
+        r) TARGET=$OPTARG ;;
+        s) SEXCHECK=1 ;;
+    esac
+done
 
 # make temp for FMR
 coord="${FMR##*:}"
@@ -32,31 +33,31 @@ rm temp.pileup.tsv
 
 coord="${COL1A1##*:}"
 chr="${COL1A1%:*}"
-seq ${coord%-*} 1000 ${coord##*-} | awk -v chr=$chr '{print chr,$1,$1+1}' | tr ' ' '\t' > temp2.positions
+seq ${coord%-*} 1000 ${coord##*-} | awk -v chr=$chr '{print chr,$1,$1+1}' | tr ' ' '\t' > temp.positions
 
 region=$COL1A1
 
-samtools mpileup -r $region --positions temp2.positions --ff SUPPLEMENTARY,UNMAP,SECONDARY,QCFAIL,DUP -q 1 -Q 1 --output-extra "PS,HP" $INPUT > temp2.pileup.tsv
+samtools mpileup -r $region --positions temp.positions --ff SUPPLEMENTARY,UNMAP,SECONDARY,QCFAIL,DUP -q 1 -Q 1 --output-extra "PS,HP" $INPUT > temp.pileup.tsv
 
-COL1A1_RESULTS=$( cut -f8 temp2.pileup.tsv | $RUSTSCRIPT )
+COL1A1_RESULTS=$( cut -f8 temp.pileup.tsv | $RUSTSCRIPT )
 
-#rm temp.positions
-#rm temp.pileup.tsv
+rm temp.positions
+rm temp.pileup.tsv
 
 ###make temp for $TARGET
 
 coord="${TARGET##*:}"
 chr="${TARGET%:*}"
-seq ${coord%-*} 1000 ${coord##*-} | awk -v chr=$chr '{print chr,$1,$1+1}' | tr ' ' '\t' > temp3.positions
+seq ${coord%-*} 1000 ${coord##*-} | awk -v chr=$chr '{print chr,$1,$1+1}' | tr ' ' '\t' > temp.positions
 
 region=$TARGET
 
-samtools mpileup -r $region --positions temp3.positions --ff SUPPLEMENTARY,UNMAP,SECONDARY,QCFAIL,DUP -q 1 -Q 1 --output-extra "PS,HP" $INPUT > temp3.pileup.tsv
+samtools mpileup -r $region --positions temp.positions --ff SUPPLEMENTARY,UNMAP,SECONDARY,QCFAIL,DUP -q 1 -Q 1 --output-extra "PS,HP" $INPUT > temp.pileup.tsv
 
-TARGET_RESULTS=$( cut -f8 temp3.pileup.tsv | $RUSTSCRIPT )
+TARGET_RESULTS=$( cut -f8 temp.pileup.tsv | $RUSTSCRIPT )
 
-#rm temp.positions
-#rm temp.pileup.tsv
+rm temp.positions
+rm temp.pileup.tsv
 
 
 ###output results
